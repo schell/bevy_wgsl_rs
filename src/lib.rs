a521a7e085b7f76505bfdafd16015db0228b5c14
Natalie Cuthbert <natalie@cuthbert.co.za>
2026-04-23 11:18:24 +0200

    Added load macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,47 @@
 use bevy::app::App;
-use bevy::asset::AssetPath;
-use bevy::asset::io::embedded::EmbeddedAssetRegistry;
+use bevy::asset::io::embedded::{EmbeddedAssetRegistry, GetAssetServer};
+use bevy::asset::{AssetPath, Handle};
+use bevy::shader::Shader;
 pub use bevy_wgsl_rs_macros::*;
 use std::path::PathBuf;
 use wgsl_rs::Module;
 
+fn wgsl_shader_path<'a>(module_path: &str) -> AssetPath<'a> {
+    let module_path = module_path.replace("::", "/");
+    let module_path = format!("embedded://{module_path}.wgsl");
+    let module_path = AssetPath::from_path_buf(module_path.into());
+    module_path
+}
 pub fn embed_wgsl_rs_shader_impl<'a>(
     app: &mut App,
     module_path: &str,
     wgsl_module: &Module,
 ) -> AssetPath<'a> {
-    let module_path = module_path.replace("::", "/");
-    let module_path = format!("embedded://{module_path}.wgsl");
-    let module_path = AssetPath::from_path_buf(module_path.into());
     let shader_source = wgsl_module.wgsl_source().join("\n");
     let embedded = app.world_mut().resource_mut::<EmbeddedAssetRegistry>();
     embedded.insert_asset(
         PathBuf::from(""),
-        module_path.path(),
+        wgsl_shader_path(module_path).path(),
         shader_source.into_bytes(),
     );
-    module_path
+    wgsl_shader_path(module_path)
+}
+
+pub fn load_wgsl_rs_shader_impl<'a>(app: &mut App, module_path: &str) -> Handle<Shader> {
+    app.get_asset_server().load(wgsl_shader_path(module_path))
 }
 
 #[macro_export]
 macro_rules! embed_wgsl_rs_shader {
     ($app: expr, $module: path) => {
         $crate::embed_wgsl_rs_shader_impl($app, &$module::MODULE_PATH, &$module::WGSL_MODULE)
+    };
+}
+
+#[macro_export]
+macro_rules! load_wgsl_rs_shader {
+    ($app: expr, $module: path) => {
+        $crate::load_wgsl_rs_shader_impl($app, &$module::MODULE_PATH)
     };
 }
 
@@ -43,6 +58,7 @@ mod tests {
     use bevy::diagnostic::DiagnosticsPlugin;
     use bevy::prelude::{BevyError, World};
     use bevy::reflect::TypePath;
+    use bevy::shader::ShaderLoader;
     use wgsl_rs::wgsl;
 
     #[bevy_wgsl]
@@ -72,49 +88,44 @@ mod tests {
         }
     }
 
-    #[derive(Asset, TypePath, Debug)]
-    pub struct ShaderText(String);
-
-    #[derive(Default, TypePath)]
-    struct ShaderTextAssetLoader;
-
-    impl AssetLoader for ShaderTextAssetLoader {
-        type Asset = ShaderText;
-        type Settings = ();
-        type Error = BevyError;
-
-        async fn load(
-            &self,
-            reader: &mut dyn Reader,
-            settings: &Self::Settings,
-            load_context: &mut LoadContext<'_>,
-        ) -> Result<Self::Asset, Self::Error> {
-            let mut bytes = Vec::new();
-            reader.read_to_end(&mut bytes).await?;
-
-            Ok(ShaderText(String::from_utf8(bytes)?))
-        }
-    }
-
     fn get_asset<A: Asset>(world: &World, id: AssetId<A>) -> Option<&A> {
         world.resource::<Assets<A>>().get(id)
     }
 
     #[tokio::test]
-    async fn test_macro() {
+    async fn test_embed_macro() {
         let mut app = App::new();
         app.add_plugins((
             TaskPoolPlugin::default(),
             AssetPlugin::default(),
             DiagnosticsPlugin,
-        ));
-        app.init_asset::<ShaderText>();
-        app.register_asset_loader(ShaderTextAssetLoader);
+        ))
+        .init_asset::<Shader>()
+        .register_asset_loader(ShaderLoader);
+
         let asset_path = embed_wgsl_rs_shader!(&mut app, hello_triangle);
-        let shader_text_handle: Handle<ShaderText> = {
+        let shader_text_handle: Handle<Shader> = {
             let asset_server = app.get_asset_server();
             asset_server.load(asset_path)
         };
+        app.update();
+
+        let shader_text = get_asset(app.world(), shader_text_handle.id());
+        assert!(matches!(shader_text, Some(_)));
+    }
+
+    #[tokio::test]
+    async fn test_load_macro() {
+        let mut app = App::new();
+        app.add_plugins((
+            TaskPoolPlugin::default(),
+            AssetPlugin::default(),
+            DiagnosticsPlugin,
+        ))
+        .init_asset::<Shader>()
+        .register_asset_loader(ShaderLoader);
+        let asset_path = embed_wgsl_rs_shader!(&mut app, hello_triangle);
+        let shader_text_handle: Handle<Shader> = load_wgsl_rs_shader!(&mut app, hello_triangle);
         app.update();
 
         let shader_text = get_asset(app.world(), shader_text_handle.id());
